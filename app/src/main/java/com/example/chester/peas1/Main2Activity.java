9e22c6bfef9247327746967b8bc8eed934c7f8d4
LAPTOP-AC7KUAJ6\Isura <ikumarapeli@gmail.com>
2018-02-24 15:45:10 -0800

    add signup activity

--- app/src/main/java/com/example/chester/peas1/Main2Activity.java
+++ app/src/main/java/com/example/chester/peas1/Main2Activity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -11,11 +13,23 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        SetupSignUpButton();
 
 
     }
 
     public static Intent makeIntent(Context context) {
         return new Intent(context, Main2Activity.class);
+    }
+
+    private void SetupSignUpButton() {
+        Button btn = (Button) findViewById(R.id.signUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =signUp.makeIntent(Main2Activity.this);
+                startActivity(intent);
+            }
+        });
     }
 }
