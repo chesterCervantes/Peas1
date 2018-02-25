75213cf350070f2779fddc7b8335d5372e717381
LAPTOP-AC7KUAJ6\Isura <ikumarapeli@gmail.com>
2018-02-24 20:44:25 -0800

    me commit this

--- app/src/main/java/com/example/chester/peas1/signUp.java
+++ app/src/main/java/com/example/chester/peas1/signUp.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class signUp extends AppCompatActivity {
 
@@ -11,9 +13,24 @@ public class signUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        SetupSignInButton();
+
     }
+
+
 
     public static Intent makeIntent(Context context) {
         return new Intent(context, signUp.class);
+    }
+
+    private void SetupSignInButton() {
+        Button btn = (Button) findViewById(R.id.btn_SignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =SignInAct.makeIntent(signUp.this);
+                startActivity(intent);
+            }
+        });
     }
 }
