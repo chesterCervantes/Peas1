a36fb883e77a3b97d606615b887306dab1cc0754
Chester <ccervant@sfu.ca>
2018-02-24 21:36:32 -0800

    Add AWSDB

---
+++ app/src/main/java/com/example/chester/peas1/Database/AWS_DB.java
@@ -0,0 +1,10 @@
+package com.example.chester.peas1.Database;
+
+
+/**
+ * Created by Chester on 2018-02-24.
+ */
+
+public class AWS_DB {
+
+}
--- app/src/main/java/com/example/chester/peas1/MainActivity.java
+++ app/src/main/java/com/example/chester/peas1/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.amazonaws.mobile.client.AWSMobileClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        AWSMobileClient.getInstance().initialize(this).execute();
         SetupStartButton();
     }
     // Test
