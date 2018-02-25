a91f62e3bd9a6b0320ee30aa4e201268998fdc91
andreyvarlamov <andreyvarlamov@github.com>
2018-02-24 16:11:52 -0800

    added trip and user classes

---
+++ app/src/main/java/com/example/chester/peas1/model/Trip.java
@@ -0,0 +1,11 @@
+package com.example.chester.peas1.model;
+
+/**
+ * Created by andreyv on 2018-02-24.
+ */
+
+public class Trip {
+    public int startTime = 0;// unix time
+    public int arrivalTime = 0; //unix time
+    public
+}
---
+++ app/src/main/java/com/example/chester/peas1/model/User.java
@@ -0,0 +1,14 @@
+package com.example.chester.peas1.model;
+
+/**
+ * Created by andreyv on 2018-02-24.
+ */
+
+public class User {
+    private String password = "";
+    private String email = "";
+    private String firstName = "";
+    private String lastName = "";
+
+
+}
