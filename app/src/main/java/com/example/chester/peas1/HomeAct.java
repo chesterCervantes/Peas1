415072645a3dea6c671fcbdea816b478abe20510
LAPTOP-AC7KUAJ6\Isura <ikumarapeli@gmail.com>
2018-02-25 00:28:45 -0800

    me commit this 2

--- app/src/main/java/com/example/chester/peas1/HomeAct.java
+++ app/src/main/java/com/example/chester/peas1/HomeAct.java
@@ -1,14 +1,18 @@
 package com.example.chester.peas1;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeAct extends AppCompatActivity {
-
+    public static final int REQUEST_CODE_GET_ARRIVAL_TIME = 10;
+    private int hours;
+    private int min;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,10 +44,23 @@ public class HomeAct extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent =SelectTime.makeIntent(HomeAct.this);
-                startActivity(intent);
+                startActivityForResult(intent,REQUEST_CODE_GET_ARRIVAL_TIME);
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    switch (requestCode){
+        case REQUEST_CODE_GET_ARRIVAL_TIME:
+        if (resultCode== Activity.RESULT_OK){
+            hours=data.getIntExtra("timeHours",0);
+            min=data.getIntExtra("timeMin",0);
 
+        }
+            Toast.makeText(HomeAct.this ,"test "+hours+" "+min,Toast.LENGTH_LONG).show();
+
+    }
+
+    }
 }
--- app/src/main/java/com/example/chester/peas1/SelectTime.java
+++ app/src/main/java/com/example/chester/peas1/SelectTime.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TimePicker;
 
 public class SelectTime extends AppCompatActivity {
 
@@ -13,13 +14,32 @@ public class SelectTime extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_time);
-
+        addActivity();
     }
 
     public static Intent makeIntent(Context context) {
         return new Intent(context, SelectTime.class);
     }
 
+    private void addActivity (){
+        Button button = (Button ) findViewById(R.id.btn_AcceptTime);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePicker simpleTimePicker=(TimePicker)findViewById(R.id.timePicker);
+                int hours =simpleTimePicker.getHour();
+                int min =simpleTimePicker.getMinute();
+
+                Intent intent = new Intent();
+                intent.putExtra("timeHours" , hours);
+                intent.putExtra("timeMin" , min);
+                setResult(HomeAct.RESULT_OK,intent);
+                finish();
+
+            }
+
+        });
+    }
 
 
 
